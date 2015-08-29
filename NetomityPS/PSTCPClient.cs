ddf87547f2cb20b3fa9ca880ab39c563a453cca1
zonyl <jason@sharpee.com>
2015-08-29 13:12:40 -0400

    Add new TCPClient powershell CMDLET

---
+++ NetomityPS/PSTCPClient.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using Netomity.Interfaces.Basic;
+using Netomity.Core;
+
+namespace NetomityPS
+{
+    [Cmdlet(VerbsCommon.New, "TCPClient")]
+    public class PSTCPClient: PSCommon
+    {
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            ValueFromPipeline = true,
+            Position = 0,
+            HelpMessage = "Address"
+        )]
+        public string Address { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            ValueFromPipeline = true,
+            Position = 0,
+            HelpMessage = "Port Number"
+        )]
+        public int PortNumber { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            ValueFromPipelineByPropertyName = true,
+            ValueFromPipeline = true,
+            Position = 0,
+            HelpMessage = "Name"
+        )]
+        public string Name { get; set; }
+
+
+        protected override void ProcessRecord()
+        {
+            WriteVerbose(String.Format("TCPClient - Port:{0}", PortNumber));
+            WriteObject(new TCPClient(hostName: Address, port: PortNumber) { Name = Name });
+        }
+    }
+}
